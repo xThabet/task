004b1a9f00e670b65179c2e167e1e7e59f1b7e96
dev-8 <thabet.atef@sociumtech.com>
2024-08-28 16:09:32 +0300

    Thabet task

--- task.py
+++ task.py
@@ -8,21 +8,28 @@ def is_palindrome(s):
         char_freq[char] = char_freq.get(char, 0) + 1
     return is_pal, char_freq
 
-import math
+
+#function to check if the number is prime 
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     if n <= 3:
+#         return True
+#     if n % 2 == 0 or n % 3 == 0:
+#         return False
+#     i = 5
+#     while i * i <= n:
+#         if n % i == 0 or n % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
+
 
 def is_prime(n):
     if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+        return [(False, i) for i in range(2, int(math.sqrt(n)) + 1)]
+    
+    return [(n % i == 0, i) for i in range(2, int(math.sqrt(n)) + 1)]
 
 
 def main():
